986766156776ef83b14a6b329a3d4dc183037abe
JakeHildy <jacob.hildebrandt@gmail.com>
2021-07-21 15:11:53 -0700

    Testing Transacations on Ropsten with no success

--- index.js
+++ index.js
@@ -1,42 +1,18 @@
-const { Buffer } = require("buffer");
+const { sendTransaction, getAccountBalance } = require("./utils/ethAPI");
+const {
+  mmAcc1,
+  mmAcc2,
+  mmAcc1PrivateKey,
+  mmAcc2PrivateKey,
+} = require("./keys");
 
-// Step 1 - Configuration
-const Web3 = require("web3");
-const EthereumTransaction = require("ethereumjs-tx").Transaction;
-const url = "http://127.0.0.1:7545";
-const web3 = new Web3(url);
+const sendingAddress = "0x3E8a3803a3850c2EB1260ad1a389422Ca33173Ca";
+const privateKey =
+  "5ff1ac4a8aafca5e2a94a4ef210d9d679e4487a2eb5e82e457bec07a8fbd2230";
+const receivingAddress = "0x2047772d6588aAf463f45e9e493dB91Ef1242708";
+const amount = 0.5;
 
-// Step 2 - Set the sending and receiving addresses for the transaction.
-const sendingAddress = "0xe8766e70aFC602cd4Ac224f3AE8D8b17D565e756";
-const receivingAddress = "0x3E8a3803a3850c2EB1260ad1a389422Ca33173Ca";
-
-// Step 3 - Check the balances of each address
-// web3.eth.getBalance(sendingAddress).then(console.log);
-// web3.eth.getBalance(receivingAddress).then(console.log);
-
-// small change to test git config
-const amountETH = "2";
-const amountWei = web3.utils.toWei(amountETH);
-
-// === CREATE TRANSACTION ===
-const rawTransaction = {
-  nonce: "0x05",
-  to: receivingAddress,
-  gasPrice: "0x20000000",
-  gasLimit: "0x30000",
-  value: web3.utils.numberToHex(amountWei),
-  data: "0x0000",
-};
-
-// === SIGN TRANSACTION ===
-const privateKeySender =
-  "e0f609479304c8732004d2ea8a21e97664cdf7f99a94cc9a47c538e4eadc38c0";
-
-const privateKeySenderHex = Buffer.from(privateKeySender, "hex");
-// good to here.
-const transaction = new EthereumTransaction(rawTransaction);
-transaction.sign(privateKeySenderHex);
-
-// === SEND THE TRANSACTION TO THE NETWORK ===
-const serializedTransaction = transaction.serialize();
-web3.eth.sendSignedTransaction(serializedTransaction);
+sendTransaction(sendingAddress, receivingAddress, amount, privateKey);
+// sendTransaction(mmAcc1, mmAcc2, amount, mmAcc1PrivateKey);
+// getAccountBalance(mmAcc1);
+// getAccountBalance(mmAcc2);
---
+++ utils/ethAPI.js
@@ -0,0 +1,57 @@
+const EthereumTransaction = require("ethereumjs-tx").Transaction;
+const Web3 = require("web3");
+const url = "http://127.0.0.1:7545";
+const ropstenUrl =
+  "https://ropsten.infura.io/v3/0eed1fcfd57c445796f1cbb23fbb2a44";
+const web3 = new Web3(url);
+
+/* ===== Get Account Balance ====================
+|  Gets the account Balance                     |
+/* ============================================*/
+exports.getAccountBalance = async (address) => {
+  //   return new Promise((resolve, reject) => {
+  //     web3.eth.getBalance(address, (err, bal) => {
+  //       if (err) reject(err);
+  //       resolve(web3.utils.fromWei(bal, "ether"));
+  //     });
+  //   });
+  web3.eth.getBalance(address, (err, bal) => {
+    if (err) console.log(err);
+    else console.log(web3.utils.fromWei(bal, "ether"));
+  });
+};
+
+/* ===== Send Transaction =======================
+|  Sends a transaction on local blockchain      |
+/* ============================================*/
+exports.sendTransaction = async (
+  sendingAddress,
+  receivingAddress,
+  eth,
+  privateKey
+) => {
+  const amountWei = web3.utils.toWei(eth.toString());
+
+  // Get Transaction Count
+  const rawTxCount = await web3.eth.getTransactionCount(sendingAddress);
+  const txCount = web3.utils.numberToHex(rawTxCount);
+
+  // === CREATE TRANSACTION ===
+  const rawTransaction = {
+    nonce: txCount,
+    to: receivingAddress,
+    gasPrice: "0x20000000",
+    gasLimit: "0x30000",
+    value: web3.utils.numberToHex(amountWei),
+    data: "0x0000",
+  };
+
+  // === SIGN TRANSACTION ===
+  const privateKeyHex = Buffer.from(privateKey, "hex");
+  const transaction = new EthereumTransaction(rawTransaction);
+  transaction.sign(privateKeyHex);
+
+  // === SEND TRANSACTION ===
+  const serializedTransaction = transaction.serialize();
+  web3.eth.sendSignedTransaction(serializedTransaction);
+};
